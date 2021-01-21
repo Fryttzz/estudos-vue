34c2d241e6a23cc67fae5b29056bb635de381d89
Matheus Santos <matheusps0012@gmail.com>
2021-01-21 18:33:34 -0300

    In progress - home page

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+      <h3>Test</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+.header {
+    display: flex;
+    flex-direction: row;
+    padding-left: 10px;
+    background-color: #27313b;
+}
+</style>
